146bd71d04c93ee56801258a6413718210d61dfb
Sharkalien <leonardodanielescoto@gmail.com>
2024-12-28 09:01:14 -0600

    Update DemoComputerRoom.tscn

--- Project/Rooms/DemoRoom/pooplordexeExtraFunc.gd
+++ Project/Rooms/DemoRoom/pooplordexeExtraFunc.gd
@@ -1,10 +1,18 @@
 extends Node
 
-onready var colors = get_parent() as Polygon2D
+export(NodePath) onready var darkness = get_node(darkness) as Polygon2D
+onready var computer_light = get_parent() as Polygon2D
 
 func extraFunc():
-	colors.texture.gradient.set_colors([Color(1,0,0,0), Color(1,0,0,0.635294)])
+	computer_light.texture.gradient.set_colors([Color(1,0,0,0), Color(1,0,0,0.635294)])
 	Global.audioNode.set_pitch_scale(0.3)
+	
+	if !computer_light.visible:
+		computer_light.show()
+	if !darkness.visible:
+		darkness.show()
+	if !Global.audioNode.playing:
+		Global.audioNode.play()
 
 func _exit_tree() -> void:
 	Global.audioNode.set_pitch_scale(1.0)
